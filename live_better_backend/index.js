69d7657a9c6a4fcd81b2cb243a125b519a68c14f
vishalkumar48815 <vk9473371@gmail.com>
2024-04-23 00:25:35 +0530

    Created signup api and added first data to the database

--- live_better_backend/index.js
+++ live_better_backend/index.js
@@ -2,6 +2,7 @@ import express from 'express' ;
 import mongoose from 'mongoose' ;
 import dotenv from 'dotenv';
 import userRoute from './routes/user.route.js' ;
+import authRouter from './routes/auth.route.js' ;
 
 dotenv.config();
 
@@ -16,9 +17,11 @@ mongoose.connect(process.env.MONGO)
 let app = express();
 const port = 5000 ;
 
+app.use(express.json()) ;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}.`)
 })
 
 app.use('/api/user', userRoute) ;
+app.use('/api/auth', authRouter) ;
